7f16f45d5e6dca124ab2946f1d252be83ea07a58
Hailey Montgomery <hailoh@hotmail.com>
2016-11-29 00:00:31 +0000

    added gun

--- js/game.js
+++ js/game.js
@@ -418,12 +418,13 @@ function checkAnswer(){
 }
 
 }
- var background = new Image();
-  background.src = "Images/game bg.jpg";
+ 
+
 //This function is Used to draw all the images, the background and the entire game
 var drawScene = function() {
-
  
+  var background = new Image();
+  background.src = "Images/game bg.jpg";
   var bubble1 = new Image();
   bubble1.src = "Images/new bubble.png";
   var bubble2 = new Image();
@@ -433,7 +434,7 @@ var drawScene = function() {
   var bubble4 = new Image();
   bubble4.src = "Images/new bubble.png";
   var character = new Image();
-  character.src = "Images/gunesdvedf.png";
+  character.src = "Images/gun.png";
 
   ctx.drawImage(background,0,0,1280,600);
   ctx.drawImage(bubble1, bubblex,200,80,80);
